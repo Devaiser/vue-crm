d18635a0597ed162334e5b1143d9ef3c85f9ad1b
Denis Lozko <denis.lozko@cirus-it.com>
2022-06-09 00:43:26 +0300

    Added pagination

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import Paginate from 'vuejs-paginate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -26,6 +27,7 @@ Vue.filter('currency', currencyFilter);
 Vue.directive('tooltip', tooltipDirective);
 
 Vue.component('Loader', Loader);
+Vue.component('Paginate', Paginate);
 
 firebase.initializeApp({
   apiKey: 'AIzaSyA-hvn2IUAGxMOcZCDnqgFWJe-sU5w09CE',
